413f2ffd8c09b56a61c8ebfd94ad8a9ac731d886
Abdullah Ayman <abdulluhayman@gmail.com>
2025-03-12 00:10:36 +0200

    Added the User Controller for login and Signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,52 +3,61 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const dotenv = require('dotenv').config();
-const registerUser = asyncHandler(async (req, res) => {
+const prisma = require('../prisma/prismaClient');
+const registerUser = async (req, res) => {
     const { username, email, password } = req.body;
-    if (!username || !email || !password) {
-        res.status(400);
-        throw new Error('Please fill in all fields');
+    try {
+        const User = await prisma.User.create({
+            data: {
+                name: username,
+                email: email,
+                password: password
+            }
+        });
+        res.status(200).json({ "message": "Success", "Registered User": User });
     }
-    const userAvailable = await User.findOne({ email });
-    if (userAvailable) {
-        res.status(400);
-        throw new Error('User is already found');
+    catch (err) {
+        res.status(500).json({ "Message": err.message });
     }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log("Hashed Password " + hashedPassword);
-    const createdUser = await User.create({ username, email, password: hashedPassword });
-    if (createdUser) {
-        res.status(200).json({ _id: createdUser.id, "email": email });
 
-    }
-    else {
-        res.status(400);
-        throw new Error("User data is not valid")
-    }
-});
+};
 
 const currentUser = asyncHandler(async (req, res) => {
-    res.json(req.user);
+
 });
 
-const loginUser = asyncHandler(async (req, res) => {
+const loginUser = async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
-        res.status(400);
-        throw new Error("All fields are mandatory");
+        res.status(400).json({ "Message": "All fields are mandatory" });
+
     }
-    const user = await User.findOne({ email });
-    if (user && (await bcrypt.compare(password, user.password))) {
-        const token = jwt.sign({ username: user.username, email: user.email, id: user._id },
-            process.env.SECRET_KEY,
-            { expiresIn: "15m" });
-        res.status(200).json({ "token": token });
+    try {
+        const user = await prisma.User.findUnique({
+            where: {
+                email: email,
+                password: password
+            }
+        })
+        if (!user) {
+            res.status(404).json({ "Message": "Wrong username or password" });
+        }
+        res.status(200).json({ "Message": "Login successful" });
     }
-    else {
-        res.status(401);
-        throw new Error("Email or password is not valid");
+    catch (err) {
+        res.status(500).json({ "Message": err.message });
     }
 
-})
+
+
+}
 
 module.exports = { registerUser, currentUser, loginUser };
+
+
+// if (user && (await bcrypt.compare(password, user.password))) {
+//     const token = jwt.sign({ username: user.username, email: user.email, id: user._id },
+//         process.env.SECRET_KEY,
+//         { expiresIn: "15m" });
+//     res.status(200).json({ "token": token });
+// }
